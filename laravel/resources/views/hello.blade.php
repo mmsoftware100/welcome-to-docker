c08e52540af9b2701c918243f4d486da10f78ec2
Aung Ko Man <aungkoman@gmail.com>
2023-11-14 15:28:30 +0630

    update view with centered html

---
+++ laravel/resources/views/hello.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .center-content {
+            text-align: center;
+        }
+    </style>
+    <title>Hello World Centered</title>
+</head>
+<body>
+    <div class="center-content">
+        <h1>Hello, World!</h1>
+    </div>
+</body>
+</html>
